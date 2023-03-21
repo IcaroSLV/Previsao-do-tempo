38fc1201cff75501fcd885e252152e2cbfab7ac3
unknown <icaroisl6582@gmail.com>
2023-03-21 12:05:20 -0300

    FEITO O CARTAO DE PESQUISA

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   const [latitude, setLatitude] = useState()
   const [longetude, setLongetude] = useState()
-  const [cidade, setCidade] = useState( )
+  const [cidade, setCidade] = useState("")
   const [showCard, SetShowCard] = useState(false)
   const [invalidCity, setInvalidCity] = useState(false)
 
@@ -18,50 +18,24 @@ function App() {
     setLatitude( )
     setLongetude( )
     setInvalidCity(false)
+    SetShowCard(false)
 
     if(cidade.length > 0) {
       fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cidade}&limit=5&appid=36c1bc257e47292962e98d75553193b4`)
       .then(res => res.json())
       .then(data => {
-        setLatitude(data[0].lat)
-        setLongetude(data[0].lon)
-        console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longetude}&appid=36c1bc257e47292962e98d75553193b4`)
+        setLatitude(data[1].lat)
+        setLongetude(data[1].lon)
+        SetShowCard(true)
       })
-      .catch(err => console.log(err))
+      .catch(err => setInvalidCity(true))
 
     } else {
       setInvalidCity(true)
+      setLatitude()
+      setLongetude()
     }
   }
-
-  const [kelvinTemp, setKelvinTemp] = useState(0)
-  const [weatherMain, setWeatherMain] = useState()
-  const [weatherMainDescription, setWeatherMainDescription] = useState()
-  const [umidade, setUmidade] = useState()
-  const [vento, setVento] = useState()
-
-  useEffect (() => {
-
-    setKelvinTemp( )
-    setWeatherMain( )
-    setWeatherMainDescription( )
-    setUmidade( )
-    setVento( )
-    SetShowCard(false)
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longetude}&appid=36c1bc257e47292962e98d75553193b4`)
-    .then(resp => resp.json())
-    .then(data => {
-      setKelvinTemp(data.main.temp)
-      setWeatherMain(data.weather[0].main)
-      setWeatherMainDescription(data.weather[0].description)
-      setUmidade(data.main.humidity)
-      setVento(data.wind.speed)
-      SetShowCard(true)
-
-      })
-    .catch(err => console.log(err))
-  }, [latitude, longetude])
 
   return (
     <div className="App" onSubmit={onSubmit}>
@@ -73,14 +47,13 @@ function App() {
           {invalidCity? <div className="invalid">INSIRA UMA CIDADE VÁLIDA</div> : <div></div>}
           <button type="submit">Procurar</button>   
       </form>
-      {showCard? <WeatherCard 
-        kelvinTemp={kelvinTemp} 
-        weatherMain={weatherMain}
-        weatherMainDescription={weatherMainDescription}
-        humidity={umidade}
-        wind={vento}
-        
-      /> : <Loading />}
+      {longetude && (
+        <WeatherCard 
+          nomeDaCidade={cidade}
+          longetude={longetude}
+          latitude={latitude}
+        />
+      )}
     </div>
   );
 }
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,23 +1,86 @@
 import { useEffect, useState } from "react"
+import styles from "./WeatherCard.module.css"
+import WindImage from '../images/vento.png'
+import clima from '../images/clima.webp'
 
-function WeatherCard({kelvinTemp, wind, humidity, weatherMain, weatherMainDescription}) {
+function WeatherCard({nomeDaCidade, latitude, longetude}) {
+
+
+    // PEGAR OS DADOS DA CIDADE
     
-    const [celciusTemp, setCelciusTemp] = useState()
-    const [fahrenheitTemp, setFahrenheitTemp] = useState()
+    const [cityName, setCityName] = useState()
+    const [kelvinTemp, setKelvinTemp] = useState(0)
+    const [weatherMain, setWeatherMain] = useState()
+    const [weatherMainDescription, setWeatherMainDescription] = useState()
+    const [umidade, setUmidade] = useState()
+    const [Wind, setWind] = useState()
+    const [iconId, setIconId] = useState()
+  
+    useEffect (() => {
+  
+      setKelvinTemp( )
+      setWeatherMain( )
+      setWeatherMainDescription( )
+      setUmidade( )
+      setWind( )
+  
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longetude}&appid=36c1bc257e47292962e98d75553193b4`)
+      .then(resp => resp.json())
+      .then(data => {
+        setKelvinTemp(data.main.temp)
+        setWeatherMain(data.weather[0].main)
+        setWeatherMainDescription(data.weather[0].description)
+        setUmidade(data.main.humidity)
+        setWind(data.wind.speed)
+        setCityName(nomeDaCidade)
+        setIconId(data.weather[0].icon)
+        
+  
+        })
+      .catch(err => console.log(err))
+    }, [latitude, longetude])
 
-    const teste = () => {
-        console.log(kelvinTemp, weatherMain,weatherMainDescription,wind,humidity)
-    }
 
-    if(kelvinTemp > 0) {
-        return(
-            <button onClick={teste}>CLICKAR</button>
-        )
-    }
+
+    // PEGAR O DIA DA SEMANA 
+
+    const diasDaSemana = ["DOMINGO", "SEGUNDA-FEIRA", "TERÇA-FEIRA", "QUARTA-FEIRA", "QUINTA-FEIRA", "SEXTA-FEIRA", "SÁBADO"]
+    const Today = new Date()
+    const diaDaSemana = diasDaSemana[Today.getDay()]
+
+    const [currentTemp, setCurrentTemp] = useState(0)
+
 
     return(
         <div>
-            
+            <div className={styles.Card}>
+                <section className={styles.weatherDescription}>
+                    <h1>{nomeDaCidade.toUpperCase()}</h1>
+                    <div >
+                        <div>
+                            <div><img src={WindImage}></img>Vento</div>
+                            <p>{Wind} <span>m/s</span></p>
+                        </div>
+                        <div>
+                            <div> <img src={iconId && `https://openweathermap.org/img/wn/${iconId}@2x.png`}></img>Clima</div>
+                            <p>{weatherMain}</p>
+                        </div>
+                        <div>
+                            <div><img src={clima}></img>Descriçao do Clima:</div>
+                            <p>{weatherMainDescription}</p>
+                        </div>
+                    </div>
+                </section>
+                <section className={styles.tempDescription}>
+                    <div>{diaDaSemana}</div>
+                    {kelvinTemp && (
+                        <section>
+                            <div>{(kelvinTemp - 273).toFixed(1)} <span>°C</span></div>
+                            <div>{((kelvinTemp - 273) * 9/5 + 32).toFixed(1)} <span>°F</span></div>
+                        </section>
+                    )}
+                </section>
+            </div>
         </div>
     )
 }
